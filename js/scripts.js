c429f0c98d1c273153c9f6ffcba4619ef8f06afa
Maksim <shestakov7577412@gmail.com>
2018-07-11 23:28:58 +0300

    Add eslint

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,20 @@
 var dangerZone = [
-    {top: 190, left: 200},
-    {top: 190, left: 210},
-    {top: 190, left: 220},
-    {top: 190, left: 220},
-    {top: 200, left: 220},
-    {top: 220, left: 220},
-    {top: 230, left: 220},
-    {top: 230, left: 210},
-    {top: 230, left: 200},
-    {top: 230, left: 190},
-    {top: 220, left: 190},
-    {top: 210, left: 190},
-    {top: 200, left: 190},
-    {top: 190, left: 190},
-
-
-
-
-
+    { top: 190, left: 200 },
+    { top: 190, left: 210 },
+    { top: 190, left: 220 },
+    { top: 190, left: 220 },
+    { top: 200, left: 220 },
+    { top: 220, left: 220 },
+    { top: 230, left: 220 },
+    { top: 230, left: 210 },
+    { top: 230, left: 200 },
+    { top: 230, left: 190 },
+    { top: 220, left: 190 },
+    { top: 210, left: 190 },
+    { top: 200, left: 190 },
+    { top: 190, left: 190 }
 ];
+
 var step = 10;
 var user = $('.user');
 
@@ -28,14 +24,14 @@ var RIGHT = 39;
 var DOWN = 40;
 var help = {
     create_block: function(e) {
-        
+
     },
 
     current_block: function(offset) {
-        var c_top = offset.top; //205
-        var c_left = offset.left; //205
+        var c_top = offset.top;
+        var c_left = offset.left;
         var block_steps = [];
-        $.each(dangerZone, function( index, value ) {
+        $.each(dangerZone, function(index, value) {
             if (value.top == c_top + step && value.left == c_left) block_steps.push(DOWN);
             if (value.top == c_top - step && value.left == c_left) block_steps.push(UP);
             if (value.top == c_top && value.left == c_left + step) block_steps.push(RIGHT);
@@ -45,7 +41,7 @@ var help = {
     },
 
     draw_blocks: function() {
-        $.each(dangerZone, function( index, value ) {
+        $.each(dangerZone, function(index, value) {
             var block = $('<div>', {class: 'block'});
             block.offset(value);
             $('.start').append(block);
@@ -53,10 +49,9 @@ var help = {
     }
 };
 
-
 $(document).ready(function() {
     help.draw_blocks();
-    $(document).keydown(function(e){
+    $(document).keydown(function(e) {
         var offset = user.offset();
         var current_block = help.current_block(offset);
         if (e.keyCode == LEFT && !current_block.includes(LEFT)) {
@@ -67,6 +62,6 @@ $(document).ready(function() {
             user.offset({ top: offset.top, left: offset.left + step });
         } else if (e.keyCode == DOWN && !current_block.includes(DOWN)) {
             user.offset({ top: offset.top + step, left: offset.left });
-        } 
+        }
     });
 });
